497886660dbd5c88e203027cce0902c0b54fd67c
shate <tomashawro.kontakt@gmail.com>
2022-12-28 14:43:31 +0100

    Added dwt features (with freq info) and cwt img generation

--- msr/signals/features.py
+++ msr/signals/features.py
@@ -26,10 +26,10 @@ def calculate_statistics(arr):
 
 
 def calculate_zerocross_feats(arr, thr=0):
-    zerocross_idxs = np.nonzero(np.diff(x > thr))[0]
+    zerocross_idxs = np.nonzero(np.diff(arr > thr))[0]
 
     first_zc_idx = max(0, zerocross_idxs[0] - 1)
-    if x[first_zc_idx + 1] - x[first_zc_idx] > 0:  # first zc is from minus to plus
+    if arr[first_zc_idx + 1] - arr[first_zc_idx] > 0:  # first zc is from minus to plus
         pos_zc_idxs = zerocross_idxs[::2]
         neg_zc_idxs = zerocross_idxs[1::2]
     else:
@@ -68,3 +68,40 @@ def extract_dwt_features(signal, wavelet="db5", level=None):
     for name, coeff in zip(names, coeffs):
         features[name] = get_basic_signal_features(coeff)
     return features
+
+
+def get_dwt_freqs_and_coeffs(time, data, wavelet="db5", level=None):
+    if level is None:
+        level = pywt.dwt_max_level(len(data), wavelet)
+    dt = time[1] - time[0]
+    coeffs = pywt.wavedec(data, wavelet, level=level)
+    scales = [2 ** (level - 1)] + [2 ** (level - i) for i in range(1, level + 1)]
+    freqs = np.array([pywt.scale2frequency(wavelet, scale) / dt for scale in scales])
+    return freqs.round(4), coeffs
+
+
+def extract_dwt_features(time, data, wavelet="db5", level=None):
+    freqs, coeffs = get_dwt_freqs_and_coeffs(time, data, wavelet, level)
+    level = len(freqs) - 1
+    names = [f"approximation_{level}_freq_{freqs[0]}"] + [
+        f"detail_{level - i}_freq_{freqs[i + 1]}" for i in range(level)
+    ]
+    features = {}
+    for name, coeff in zip(names, coeffs):
+        features[name] = get_basic_signal_features(coeff)
+    return features
+
+
+def get_cwt_img(time, data, freq_dim_scale=256, wavelet="morl", plot=True):
+    """Continuous wavelet transform"""
+    dt = time[1] - time[0]
+    freq_centre = pywt.central_frequency(wavelet)
+    cparam = 2 * freq_centre * freq_dim_scale
+    scales = cparam / np.arange(1, freq_dim_scale + 1, 1)
+    cwt_matrix, freqs = pywt.cwt(data, scales, wavelet, dt)
+    cwt_matrix = abs(cwt_matrix)
+    if plot:
+        fig, ax = plt.subplots(figsize=(14, 8))
+        im = ax.contourf(x, freqs, cwt_matrix, cmap=plt.cm.seismic)
+        fig.colorbar(im)
+    return freqs, cwt_matrix
